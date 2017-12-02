f101a1831364f8ebe97e7671eef667e3278beb9f
Pong <pong230@gmail.com>
2017-12-02 23:50:31 +0700

    Create view for start new / cotinue

---
+++ app/src/main/java/com/devsenses/minebea/fragment/NewWorkFragment.java
@@ -0,0 +1,31 @@
+package com.devsenses.minebea.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.devsenses.minebea.R;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItem;
+
+/**
+ * Created by USER on 2/12/2560.
+ */
+
+public class NewWorkFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.view_start_new_model, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        int position = FragmentPagerItem.getPosition(getArguments());
+//        TextView title = (TextView) view.findViewById(R.id.item_title);
+//        title.setText(String.valueOf(position));
+    }
+}
